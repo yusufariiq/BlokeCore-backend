b6b0a200d4664ad0c6483fec91de74e1676618df
Ariiq Yusuf <yusufariiq3@gmail.com>
2024-11-20 14:30:11 +0700

    add cloudinary configuration

---
+++ models/productModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const productsSchema = new mongoose.Schema({
+    name: {type: String, required:true},
+    description: {type: String, required:true},
+    price: {type: Number, required:true},
+    image: {type: Array, required:true},
+    category: {type: String, required:true},
+    subCategory: {type: String, required:true},
+    sizes: {type: Array, required:true},
+})
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 // App config
 const app = express()
@@ -11,6 +12,7 @@ connectDB()
 // Middleware
 app.use(express.json())
 app.use(cors())
+connectCloudinary()
 
 // API endpoints
 app.get('/', (req, res) => {
